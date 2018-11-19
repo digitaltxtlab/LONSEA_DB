0a7c6c5c5f6dfdd388c789e5642ace98e7986ffc
afinnemann <adam.finnemann@gmail.com>
2018-11-19 13:14:35 +0100

    shiny update - first proto

--- shiny/proto_shiny/app.R
+++ shiny/proto_shiny/app.R
@@ -1,25 +1,69 @@
+
+#loading/Installing packages
 library(pacman)
 p_load(shiny, tidyverse)
-
 p_load("stringr","ggplot2","ggExtra","lmerTest", "zoo","lubridate", "data.table")
 
 
+#not sure if working directory needs to be - I don't think so.
+#setwd("~/LONSEA_DB/shiny/proto_shiny") #For
+
+
+#reading 3 data sets - number of individuals for 3 different units.
+total  = read.csv("count_df.csv")
+fd = read.csv("count_first_division.csv")
+ho = read.csv("count_higher_officials.csv")
+
+
+#this controls the layout of the appp
 ui = fluidPage(
-  sliderInput(inputId = "num", #notice small d
-              label = "Choose a number",
-              value = 1920, min = 1920, max = 1948),
-  plotOutput(outputId  = "hist")
+  titlePanel("Lonsea App Prototype"),
+  #selectInput("Unit", "Choose which unit you wanna see:", 
+  #            choices = c("Higher Officials", "First Division", "Full Secretariat")),
+  sidebarLayout(
+    
+    sidebarPanel(
+      sliderInput("range", "Range:",
+                  min = 1920, max = 1947,
+                  value = c(1920,1947)),
+      selectInput("var", 
+                  label = "Choose unit to display",
+                  choices = c("Total Secretariat", "First Division", "Higher Officials"),
+                  selected = "Total Secretariat")
+    ), #endsidebarpanel
+    
+    mainPanel(
+      plotOutput(outputId  = "hist")
+    )#end mainpanel
+  )# end sidebarlayou
+  
+  
+  
 )
 
+#this creates a server the feeds information to the layout
 server = function(input, output) {
   
 output$hist = renderPlot({
-  setwd("~/LONSEA_DB/shiny/shiny_test")
-  da  = read.csv("")
-  hist(rnorm(input$num)) #input$num gets its value from the num inputiD
+  da <- switch(input$var, 
+                 "Total Secretariat" = total,
+                 "Higher Officials" = ho,
+                 "First Division" = fd)
+  
+  
+  
+  da %>% 
+    filter((X > input$range[1])  & (X < input$range[2])) %>% 
+    ggplot(aes(x = X, y = Count, fill = Count)) +
+    geom_col() +
+    labs( title = "Distribution of Employees", x ="Year",y = "Number of Employees")
+    
+  
+
 })
 }
 
+#runs the app
 shinyApp(ui = ui, server = server)
 
 
--- shiny/proto_shiny/count_test.R
+++ shiny/proto_shiny/count_test.R
@@ -1,4 +1,4 @@
-#### testing
+# creating df counting number of individuals for the whole secretariat
 setwd("~/LONSEA_DB/shiny/data")
 
 da = read.csv("lon_data.csv")
@@ -10,3 +10,41 @@ count = as.data.frame(colSums(tt != ""))
 names(count) = "Count"
 
 rownames(count) = str_extract_all(rownames(count),pattern = "[0-9]{4}")  
+
+setwd("~/LONSEA_DB/shiny/proto_shiny")
+
+write.csv(count,"count_df.csv")
+
+#for first dvision
+# creating df counting number of individuals for the whole secretariat
+setwd("~/LONSEA_DB/shiny/data")
+
+tt = read.csv("timetable_first_division.csv", sep = ",") 
+tt[is.na(tt)] <- ""
+tt = tt[,-c(1,2,31)]
+
+count = as.data.frame(colSums(tt != ""))
+names(count) = "Count"
+
+rownames(count) = str_extract_all(rownames(count),pattern = "[0-9]{4}")  
+
+setwd("~/LONSEA_DB/shiny/proto_shiny")
+
+write.csv(count,"count_first_division.csv")
+
+#for higher officials
+
+setwd("~/LONSEA_DB/shiny/data")
+
+tt = read.csv("timetable_higher_officials.csv", sep = ",") 
+tt[is.na(tt)] <- ""
+tt = tt[,-c(1,2,31)]
+
+count = as.data.frame(colSums(tt != ""))
+names(count) = "Count"
+
+rownames(count) = str_extract_all(rownames(count),pattern = "[0-9]{4}")  
+
+setwd("~/LONSEA_DB/shiny/proto_shiny")
+
+write.csv(count,"count_higher_officials.csv")
