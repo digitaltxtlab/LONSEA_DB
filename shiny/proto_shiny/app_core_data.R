886b1aca16e42e5eb34d7b876f0ad71549f68b83
afinnemann <adam.finnemann@gmail.com>
2019-01-20 17:48:06 +0100

    updated app: added tabs and other features.

--- shiny/proto_shiny/app_core_data.R
+++ shiny/proto_shiny/app_core_data.R
@@ -14,9 +14,12 @@ p_load("stringr","ggplot2","ggExtra","lmerTest", "zoo","lubridate", "data.table"
 df  = read.csv("lon_data.csv")
 df %>% 
   mutate(begin_date = as.Date(begin_date),
-              end_date = as.Date(end_date),
-              gender =as.factor(gender),
-              gender =recode(gender,"0"="Male","1"="Female")) -> df
+         end_date = as.Date(end_date),
+         gender = ifelse(gender == -1, 0,1),
+         gender =as.factor(gender),
+         gender =recode(gender,"0"="Male","1"="Female")) -> df #the full data set
+
+
 
 df %>% 
   filter(str_detect(fname, "Section")| #including "Section"
@@ -29,13 +32,19 @@ df %>%
          oname != "Treasury",
          oname != "Library",
          canonical_fname != "Second Division") %>% 
-  droplevels()-> higher_officials
+  droplevels()-> higher_officials #data set containing only the higher officials
 
 #this controls the layout of the appp
+
+
 ui = fluidPage(
-  titlePanel("Lonsea App Prototype"),
-  #selectInput("Unit", "Choose which unit you wanna see:", 
-  #            choices = c("Higher Officials", "First Division", "Full Secretariat")),
+  
+  navbarPage("Lonsea Shiny App", #creates multiple components within the app, a component/Page is created with "tabpanel()"
+             tabPanel("Component 1: General features",  #this section defines the first page
+             
+             
+  titlePanel("General features"),
+  
   sidebarLayout(
     
     sidebarPanel(
@@ -52,24 +61,59 @@ ui = fluidPage(
                          choices = c("All",levels(df$gender)),
                          selected = "All"),
       
-      checkboxGroupInput(inputId = "cbox_higher_off", label = "choose higher officials oname (sections),
-                        this only works if higher offcials is chosen in the 'unit to display'", #adds checkbox for higher officlas
-                         choices = c("All",levels(higher_officials$oname)),
-                         selected = "All"),
-      
-      checkboxGroupInput(inputId = "checkbox", label = "choose nationalities", #adds checkbox for nationalities
-                         choices = c("All",levels(df$nationality)),
-                         selected = "All")
+      checkboxGroupInput(inputId = "cbox_age", label = "Age control on/off. Press on to use the other age features", #adds checkbox for gender
+                         choices = c("on","off"),
+                         selected = "off")
+    
     ), #endsidebarpanel
     
     mainPanel(
-      plotOutput(outputId  = "hist")
-    )#end mainpanel
-  )# end sidebarlayou
+      plotOutput(outputId  = "hist"),
+      
+      fluidRow( #first row beneath the main plot, containing the following three inputs
+        column(3,
+               h4("Nationality control"),
+               
+               checkboxGroupInput(inputId = "checkbox", label = "choose nationalities", #adds checkbox for nationalities
+                                  choices = c("All",levels(df$nationality)),
+                                  selected = "All")
+               ),
+              
+         column(4,
+                h4("fname control"),
+                checkboxGroupInput(inputId = "cbox_higher_off", label = "choose higher officials oname (sections),
+                        this only works if higher offcials is chosen in the 'unit to display'", #adds checkbox for higher officlas
+                                   choices = c("All",levels(higher_officials$oname)),
+                                   selected = "All")),
+        
+        ) #end fluid row
+        
+      )#end mainpanel
+  
+    ) #end sidebar layout
+             ), #ends page 1
+
+  tabPanel("Component 2: Age calculations", #begins page 2
+           titlePanel("General features"),
+           
+           sidebarLayout(
+             
+             sidebarPanel(
+               sliderInput("range", "Range:", #adds a slider for years
+                           min = 1919, max = 1948,
+                           value = c(1919,1948))),
+             mainPanel(
+               #plotOutput(outputId  = "hist")
+               ) #ends main panel
+           
+             )#ends sidebar layout
+  
+           ) #ends page 2
+  
+  ) #ends navbar page
+  )# end fluid page
   
   
-  
-)
 
 #this creates a server the feeds information to the layout
 server = function(input, output) {
@@ -77,15 +121,20 @@ server = function(input, output) {
   output$hist = renderPlot({
     
     #Filters the relevant part of the secretariat based on input$var
-    if(input$var == "First Division"){
+    if(input$var == "First Division"){ 
     df %>% 
         filter(canonical_fname == "LoN First Division") -> df
     }
+    
+    
+    
+    
     if(input$var == "Higher Officials"){
       df = higher_officials
-      if(input$cbox_higher_off != "All"){
+      
+      if(input$cbox_higher_off != "All"){ #furthermore, if the cbox_higher_off argument is different from "all", then a subset based on "oname" is filtered.
         df %>% 
-          filter(oname %in% input$cbox_higher_off)
+          filter(oname %in% input$cbox_higher_off) -> df
       }
     }
   
@@ -99,7 +148,7 @@ server = function(input, output) {
         filter(gender %in% input$cbox_gender) -> df
     }
     
-    print(head(df))
+    
      
     df %>% 
        #select relevant variables for time table
